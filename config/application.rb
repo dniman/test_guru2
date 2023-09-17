4fe27014480c0c9b159bd88fdb57520a4e8594a7
Dade Niman <dade.niman@gmail.com>
2023-09-17 12:45:23 +0300

    default locale

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,8 @@ module TestGuru2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
     config.time_zone = "Moscow"
-    # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.i18n.default_locale = :ru
   end
 end
